6a87655504be048baf1312fa74216c8bf29da91a
Christoph Gruener <christophgruener@me.com>
2019-03-02 01:03:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -37,7 +37,7 @@ class Scraper
 
 
 
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -46,6 +46,7 @@ class Scraper
         puts "  Description: #{course.description}"
       end
     end
+  end
 
 
 end
