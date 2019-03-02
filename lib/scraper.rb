102f53ac4f16380daf4c1cbf2bd10b0f135f65e6
Christoph Gruener <christophgruener@me.com>
2019-03-02 00:51:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,8 +12,8 @@ class Scraper
     binding.pry
 
     doc.css('.post').each do |post|
-      course = Course.new 
-      course.title = post.css('h2').text 
+      course = Course.new
+      course.title = post.css('h2').text
       course.schedule = post.css('.date').text
       course.description = post.css('p').text
     end
